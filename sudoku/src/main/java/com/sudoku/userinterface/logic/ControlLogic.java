9fc0023c7b8234fda858fead8630f182ba1c1cbd
Tabu Hana <taboodog@hotmail.com>
2022-06-07 15:48:02 -0700

    control logic

---
+++ sudoku/src/main/java/com/sudoku/userinterface/logic/ControlLogic.java
@@ -0,0 +1,55 @@
+package com.sudoku.userinterface.logic;
+
+import java.io.IOException;
+
+import com.sudoku.constants.GameState;
+import com.sudoku.constants.Messages;
+import com.sudoku.problemdomain.IStorage;
+import com.sudoku.problemdomain.SudokuGame;
+import com.sudoku.userinterface.IUserInterfaceContract;
+
+public class ControlLogic implements IUserInterfaceContract.EventListener {
+
+  private IStorage storage;
+
+  private IUserInterfaceContract.View view;
+
+  public ControlLogic(IStorage storage, IUserInterfaceContract.View view) {
+    this.storage = storage;
+    this.view = view;
+  }
+
+  @Override
+  public void onSudokluInput(int x, int y, int input) {
+    try {
+      SudokuGame gameData = storage.getGameData();
+      int[][] newGridState = gameData.getCopyOfGridState();
+      newGridState[x][y] = input;
+
+      gameData = new SudokuGame(GameLogic.checkForCompletion(newGridState), newGridState);
+
+      storage.updateGameData(gameData);
+
+      view.updateSquare(x, y, input);
+
+      if (gameData.getGameState() == GameState.COMPLETE) {
+        view.showDialog(Messages.GAME_COMPLETE);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      view.showError(Messages.ERROR);
+    }
+  }
+
+  @Override
+  public void onDialogClick() {
+    try {
+      storage.updateGameData(GameLogic.getNewGame());
+
+      view.updateBoard(storage.getGameData());
+    } catch (IOException e) {
+      view.showError(Messages.ERROR);
+    }
+  }
+
+}
